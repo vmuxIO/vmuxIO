cd57ab448e03395f2881f5a89b69c85a7ecf75e3
Jörg Thalheim <joerg@thalheim.io>
2022-03-16 08:11:41 +0100

    cleanup moongen + flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,18 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    packages.x86_64-linux.moongen = nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix/moongen.nix {
-      nixpkgs = nixpkgs.legacyPackages.x86_64-linux;
-      linux = nixpkgs.legacyPackages.x86_64-linux.linuxPackages_5_10.kernel;
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.hello = pkgs.hello;
+    packages.x86_64-linux.moongen = pkgs.callPackage ./nix/moongen.nix {
+      linux = pkgs.linuxPackages_5_10.kernel;
     };
 
     defaultPackage.x86_64-linux = self.packages.x86_64-linux.moongen;
 
-    devShell = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+    devShell = pkgs.mkShell {
+      buildInputs = [
         self.packages.x86_64-linux.moongen
       ];
     };
