eda4968800bfcec01e162b90a2ac07da7a0cef3b
Pogobanane <aenderboy@gmx.de>
2022-04-16 12:47:23 +0000

    update moongen to custom dpdk19 version

--- nix/moongen.nix
+++ nix/moongen.nix
@@ -13,20 +13,45 @@
 , ninja
 , meson
 , bash
+, gcc8Stdenv
 }:
-
+let 
+  libmoonsrc = fetchFromGitHub {
+    owner = "vmuxIO";
+    repo = "libmoon";
+    rev = "73caf07402def6e0395bc0e158e2e637af0a72b5";
+    fetchSubmodules = true;
+    sha256 = "sha256-/dW3MtszCXfbzK0UFTf5L8MTtDlfCq4bDzKLDlwkIvI=";
+  };
+  dpdksrc = fetchFromGitHub {
+    owner = "vmuxIO";
+    repo = "dpdk";
+    rev = "54dbc2501a10b6b41abed37ff124579b15bd2871";
+    fetchSubmodules = true;
+    sha256 = "sha256-LsvhuM9zG/2MCTqpYcYjLwMcJ8yHu/4SIGISv5pqEoo=";
+  };
+in
 stdenv.mkDerivation {
   pname = "moongen";
   version = "2021.07.17";
 
-  #src = /home/peter/dev/phd/MoonGen2;
   src = fetchFromGitHub {
-    owner = "emmericp";
+    owner = "vmuxIO";
     repo = "MoonGen";
-    rev = "25c61ee76b9ca30b83ecdeef8af2c7f89625cb4e";
+    rev = "a51cdf15004df6631b23a7fae69d5661978facd4";
     fetchSubmodules = true;
-    sha256 = "sha256-UVM98DYupE1BU2+VExJpv47nOZ8Ieno1E+YIe5p73Zo=";
+    sha256 = "sha256-QPQWDV5OL86B9BtUaKCVv0Tno5TdvoHfjvm47cNka+0=";
   };
+  
+  postUnpack = ''
+    rm -r $sourceRoot/libmoon
+    cp -r ${libmoonsrc} $sourceRoot/libmoon
+    chmod -R u+w $sourceRoot/libmoon
+
+    rm -r $sourceRoot/libmoon/deps/dpdk
+    cp -r ${dpdksrc} $sourceRoot/libmoon/deps/dpdk
+    chmod -R u+w $sourceRoot/libmoon/deps/dpdk
+  '';
 
   nativeBuildInputs = [
     cmake
@@ -48,12 +73,13 @@ stdenv.mkDerivation {
     luajit
   ];
   RTE_KERNELDIR = "${linux.dev}/lib/modules/${linux.modDirVersion}/build";
-  NIX_CFLAGS_COMPILE = "-Wno-error=maybe-uninitialized -Wno-dev";
-  CFLAGS_COMPILE = "-Wno-error=maybe-uninitialized -Wno-dev";
+  NIX_CFLAGS_COMPILE = "-Wno-error=maybe-uninitialized";
+  CFLAGS_COMPILE = "-Wno-error=maybe-uninitialized";
 
   dontConfigure = true;
 
   postPatch = ''
+    ls -la ./libmoon
     patchShebangs ./libmoon/build.sh ./build.sh
     substituteInPlace ./libmoon/build.sh \
       --replace "./bind-interfaces.sh \''${FLAGS}" "echo skipping bind-interfaces.sh"
@@ -76,7 +102,7 @@ stdenv.mkDerivation {
     mkdir -p $out/lib/libmoon
     cp -r build/libmoon $out/lib/
     mkdir -p $out/lib/dpdk
-    cp -r libmoon/deps/dpdk/x86_64-native-linuxapp-gcc/lib $out/lib/dpdk
+    cp -r libmoon/deps/dpdk/x86_64-native-linux-gcc/lib $out/lib/dpdk
     mkdir -p $out/lib/luajit
     cp -r libmoon/deps/luajit/usr/local/lib $out/lib/luajit
     mkdir -p $out/lib/highwayhash
