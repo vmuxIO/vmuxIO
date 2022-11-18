cc5d497fe409f632e764bab173716cb78466891c
Kilian Mio <kilian.mioqtum.de>
2022-11-18 15:43:52 +0000

    build successful

--- flake.nix
+++ flake.nix
@@ -49,8 +49,10 @@
         owner = "oracle";
         repo = "qemu";
         rev = "b3b53245edbd399eb3ba1655d509478c76d37a8e";
-        hash = "sha256-jlSwAFWOrG6TkTEJEE3ESwEHig8OtgsJRlDUuTFeu+U=";
+        hash = "sha256-kCX2ByuJxERLY2nHjPndVoo7TQm1j4qrpLjRcs42HU4=";
+        fetchSubmodules = true;
       };
+      version = "7.1.5";
     });
   in  {
     packages = {
