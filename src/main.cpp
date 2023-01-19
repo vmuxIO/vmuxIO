1e279d029311b0f86bc74beab70408d94dce4db6
Pogobanane <aenderboy@gmx.de>
2023-01-19 18:58:38 +0000

    add destructors and error handling

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,14 @@
 #include <unistd.h>
 #include <map>
 #include <string>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <exception>
+#include <stdexcept>
 
 #include "src/vfio-consumer.hpp"
+#include "src/util.hpp"
 
 extern "C" {
   #include "libvfio-user.h"
@@ -64,15 +70,34 @@ class VfioUserServer {
     VfioUserServer() {
     }
 
+    ~VfioUserServer() {
+      int ret;
+      printf("destroying server\n");
+      vfu_destroy_ctx(vfu_ctx);
+      ret = unlink(sock.c_str());
+      if (ret < 0) {
+        warn("Could not delete %s", sock.c_str());
+      }
+      for (const auto& [idx, fd] : shm_fds) {
+        ret = close(fd);
+        if (ret < 0) {
+          warn("Could not close fd %d", fd);
+        }
+      }
+      for (const auto& [idx, page] : mem_pages) {
+        free(page);
+      }
+    }
+
     int add_regions(std::vector<struct vfio_region_info> regions) {
       int ret;
 
-      if (regions.size() > VFU_PCI_DEV_VGA_REGION_IDX - VFU_PCI_DEV_BAR0_REGION_IDX)
-        printf("Warning: got %u mappable regions, but we expect normal PCI to have %d at most", (uint)regions.size(), VFU_PCI_DEV_VGA_REGION_IDX - VFU_PCI_DEV_BAR0_REGION_IDX);
+      if (regions.size() > VFU_PCI_DEV_VGA_REGION_IDX - VFU_PCI_DEV_BAR0_REGION_IDX + 1)
+        printf("Warning: got %u mappable regions, but we expect normal PCI to have %d at most\n", (uint)regions.size(), VFU_PCI_DEV_VGA_REGION_IDX - VFU_PCI_DEV_BAR0_REGION_IDX);
       for (int i = VFU_PCI_DEV_BAR0_REGION_IDX; i <= VFU_PCI_DEV_VGA_REGION_IDX; i++) {
         ret = this->add_region(&regions[i]);
         if (ret < 0) {
-            err(EXIT_FAILURE, "failed to add dma region to vfio-user");
+            die("failed to add dma region to vfio-user");
         }
       }
 
@@ -84,7 +109,7 @@ class VfioUserServer {
     unexpected_access_callback([[maybe_unused]] vfu_ctx_t *vfu_ctx, [[maybe_unused]] char * const buf, [[maybe_unused]] size_t count, [[maybe_unused]] __loff_t offset,
                 [[maybe_unused]] const bool is_write)
     {
-      printf("Unexpectedly, a vfio register/DMA access callback was triggered.");
+      printf("Unexpectedly, a vfio register/DMA access callback was triggered.\n");
       return 0;
     }
 
@@ -92,30 +117,35 @@ class VfioUserServer {
       int ret;
 
       if (region->size == 0) {
-        return -1;
+        printf("Bar region %d skipped.\n", region->index);
+        return 0;
       }
 
       // create shared memory (file backing the memory and map it)
       int tmpfd;
+      std::string filename = this->tmpfilename;
       umask(0022);
-      if ((tmpfd = mkstemp(this->tmpfilename.data())) == -1) {
-          err(EXIT_FAILURE, "failed to create backing file");
+      if ((tmpfd = mkstemp(filename.data())) == -1) {
+          die("failed to create backing file");
       }
       this->shm_fds[region->index] = tmpfd;
       unlink(this->tmpfilename.c_str());
-      if (ftruncate(tmpfd, sizeof(region->size)) == -1) {
-          err(EXIT_FAILURE, "failed to truncate backing file");
+      if (fallocate(tmpfd, 0, region->offset, region->size) == -1) {
+          die("failed to truncate backing file");
       }
+      //if (ftruncate(tmpfd, region->size) == -1) {
+          //die("failed to truncate backing file");
+      //}
       void* sharable_page = mmap(NULL, region->size, PROT_READ | PROT_WRITE,
                               MAP_SHARED, tmpfd, 0);
       this->mem_pages[region->index] = sharable_page;
       if (sharable_page == MAP_FAILED) {
-          err(EXIT_FAILURE, "failed to mmap BAR %d", region->index);
+          die("failed to mmap BAR %d", region->index);
       }
 
       // set up dma VM<->vmux
       struct iovec bar_mmap_areas[] = {
-          { .iov_base  = (void*)0, .iov_len = region->size}, // no cb, just shmem
+          { .iov_base  = (void*)region->offset, .iov_len = region->size}, // no cb, just shmem
       };
       ret = vfu_setup_region(this->vfu_ctx, region->index,
                              region->size, &(this->unexpected_access_callback),
@@ -123,16 +153,16 @@ class VfioUserServer {
                              1, // nr. items in bar_mmap_areas
                              tmpfd, 0);
       if (ret < 0) {
-          err(EXIT_FAILURE, "failed to setup BAR region %d", region->index);
+          die("failed to setup BAR region %d", region->index);
       }
 
-      printf("Bar region %d (size 0x%x) set up.", region->index, (uint)region->size);
+      printf("Bar region %d (offset 0x%x, size 0x%x) set up.\n", region->index, (uint)region->offset, (uint)region->size);
 
       return 0;
     }
 };
 
-int main() {
+int _main() {
   int ret;
 
   printf("hello 0x%X, %d, \n", VFIO_DEVICE_STATE_V1_RESUMING, VFIOC_SECRET);
@@ -143,8 +173,9 @@ int main() {
   
   VfioConsumer vfioc;
   ret = vfioc.init();
-  if (ret < 0)
-    err(EXIT_FAILURE, "failed to initialize vfio consumer");
+  if (ret < 0) {
+    die("failed to initialize vfio consumer");
+  }
   /*return 0;*/
 
   // init vfio-user
@@ -157,18 +188,18 @@ int main() {
     VFU_DEV_TYPE_PCI
   );
   if (vfu.vfu_ctx == NULL) {
-    err(EXIT_FAILURE, "failed to initialize device emulation");
+    die("failed to initialize device emulation");
   }
   
   ret = vfu_setup_log(vfu.vfu_ctx, _log, LOG_DEBUG);
   if (ret < 0) {
-    err(EXIT_FAILURE, "failed to setup log");
+    die("failed to setup log");
   }
 
   ret = vfu_pci_init(vfu.vfu_ctx, VFU_PCI_TYPE_EXPRESS, // TODO express
                      PCI_HEADER_TYPE_NORMAL, 0); // TODO 4?
   if (ret < 0) {
-    err(EXIT_FAILURE, "vfu_pci_init() failed") ;
+    die("vfu_pci_init() failed") ;
   }
 
   vfu_pci_set_id(vfu.vfu_ctx, 0xdead, 0xbeef, 0xcafe, 0xbabe);
@@ -177,18 +208,18 @@ int main() {
 
   ret = vfu.add_regions(vfioc.regions);
   if (ret < 0)
-    err(EXIT_FAILURE, "failed to add regions");
+    die("failed to add regions");
 
   // finalize
 
   ret = vfu_realize_ctx(vfu.vfu_ctx);
   if (ret < 0) {
-      err(EXIT_FAILURE, "failed to realize device");
+      die("failed to realize device");
   }
 
   ret = vfu_attach_ctx(vfu.vfu_ctx);
   if (ret < 0) {
-      err(EXIT_FAILURE, "failed to attach device");
+      die("failed to attach device");
   }
 
   // runtime loop
@@ -202,11 +233,21 @@ int main() {
 
   if (ret == -1 &&
       errno != ENOTCONN && errno != EINTR && errno != ESHUTDOWN) {
-      errx(EXIT_FAILURE, "failed to realize device emulation");
+      die("failed to realize device emulation");
   }
 
-  // destruction
+  // destruction is done by ~VfioUserServer
 
-  vfu_destroy_ctx(vfu.vfu_ctx);
   return 0;
 }
+
+int main() {
+  try {
+    return _main();
+  } catch (...) {
+    // we seem to need this catch everyting so that our destructors work
+    return EXIT_FAILURE;
+  }
+  return 0;
+}
+
--- src/vfio-consumer.cpp
+++ src/vfio-consumer.cpp
@@ -10,6 +10,8 @@
 #include <stdlib.h>
 #include <vector>
 
+#include "src/util.hpp"
+
 int VfioConsumer::init() {
   int ret, container, group, device;
   uint32_t i;
@@ -22,59 +24,59 @@ int VfioConsumer::init() {
   /* Create a new container */
   container = open("/dev/vfio/vfio", O_RDWR);
   if (container < 0) {
-    err(EXIT_FAILURE, "Cannot open /dev/vfio/vfio");
+    die("Cannot open /dev/vfio/vfio");
   }
 
   if (ioctl(container, VFIO_GET_API_VERSION) != VFIO_API_VERSION) {
     /* Unknown API version */
-    err(EXIT_FAILURE, "VFIO version mismatch");
+    die("VFIO version mismatch");
   }
 
   if (!ioctl(container, VFIO_CHECK_EXTENSION, VFIO_TYPE1v2_IOMMU)) {
     /* Doesn't support the IOMMU driver we want. */
-    err(EXIT_FAILURE, "VFIO extension unsupported");
+    die("VFIO extension unsupported");
   }
 
   /* Open the group */
   group = open("/dev/vfio/29", O_RDWR);
   if (group < 0) {
-    err(EXIT_FAILURE, "Cannot open /dev/vfio/29");
+    die("Cannot open /dev/vfio/29");
   }
 
   /* Test the group is viable and available */
   ret = ioctl(group, VFIO_GROUP_GET_STATUS, &group_status);
   if (ret < 0) {
-    err(EXIT_FAILURE, "Cannot get vfio satus");
+    die("Cannot get vfio satus");
   }
 
   if (!(group_status.flags & VFIO_GROUP_FLAGS_VIABLE)) {
     /* Group is not viable (ie, not all devices bound for vfio) */
-    err(EXIT_FAILURE, "Some flag missing");
+    die("Some flag missing");
   }
 
   /* Add the group to the container */
   ret = ioctl(group, VFIO_GROUP_SET_CONTAINER, &container);
   if (ret < 0) {
-    err(EXIT_FAILURE, "Cannot set vfio container");
+    die("Cannot set vfio container");
   }
 
   /* Enable the IOMMU model we want */
   ret = ioctl(container, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU);
   if (ret < 0) {
-    err(EXIT_FAILURE, "Cannot set iommu type");
+    die("Cannot set iommu type");
   }
 
   /* Get addition IOMMU info */
   ret = ioctl(container, VFIO_IOMMU_GET_INFO, &iommu_info);
   if (ret < 0) {
-    err(EXIT_FAILURE, "Cannot get iommu info");
+    die("Cannot get iommu info");
   }
 
   /* Allocate some space and setup a DMA mapping */
   dma_map.vaddr = (uint64_t)(mmap(0, 1024 * 1024, PROT_READ | PROT_WRITE,
                        MAP_PRIVATE | MAP_ANONYMOUS, 0, 0));
   if (dma_map.vaddr == (uint64_t) MAP_FAILED) {
-    err(EXIT_FAILURE, "Cannot allocate memory for DMA map");
+    die("Cannot allocate memory for DMA map");
   }
   dma_map.size = 1024 * 1024;
   dma_map.iova = 0; /* 1MB starting at 0x0 from device view */
@@ -82,19 +84,19 @@ int VfioConsumer::init() {
 
   ret = ioctl(container, VFIO_IOMMU_MAP_DMA, &dma_map);
   if (ret < 0) {
-    err(EXIT_FAILURE, "Cannot set dma map");
+    die("Cannot set dma map");
   }
 
   /* Get a file descriptor for the device */
   device = ioctl(group, VFIO_GROUP_GET_DEVICE_FD, "0000:18:00.0");
   if (device < 0) {
-    err(EXIT_FAILURE, "Cannot device id for group %d", group);
+    die("Cannot device id for group %d", group);
   }
 
   /* Test and setup the device */
   ret = ioctl(device, VFIO_DEVICE_GET_INFO, &device_info);
   if (ret < 0) {
-    err(EXIT_FAILURE, "Cannot get device info for device %d", device);
+    die("Cannot get device info for device %d", device);
   }
 
   printf("\nDevice regions: %d\n\n", device_info.num_regions);
