4befd49b6e35ba4502e9441e13a0ffd784804ba2
Patrick Sabanic <patrick.sabanic@tum.de>
2023-06-28 13:43:36 +0200

    Move libvfio-user processing to different threads

---
+++ src/runner.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include "src/vfio-server.hpp"
+#include <thread>
+#include <atomic>
+#include <optional>
+class VmuxRunner{
+
+    public: 
+    VfioUserServer vfu;
+    VfioConsumer vfioc;
+    std::thread runner;
+    std::optional<Capabilities> caps;
+    std::atomic_int state;
+    std::atomic_bool running;
+    std::string device;
+    std::string socket;
+
+    enum State{
+        NOT_STARTED = 0,
+        STARTED = 1,
+        INITILIZED = 2,
+        CONNECTED = 3,
+    };
+
+    VmuxRunner(std::string socket, std::string device):
+        vfu(socket),vfioc("","")
+    {
+        state.store(0);
+        this->device = device;
+        this->socket = socket;
+    }
+        
+    void start(){
+        runner = std::thread(&VmuxRunner::run, this);
+    }
+
+    void stop(){
+        running.store(0);
+    }
+    
+    void join(){
+        runner.join();
+    }
+
+    bool is_initialized(){
+        return state == INITILIZED;
+    }
+    bool is_connected(){
+        return state == CONNECTED;
+    }
+    VfioUserServer& get_interrupts(){
+        return vfu;
+    }
+
+    private:
+    void run();
+    void add_caps();
+    void initilize();
+
+
+};
--- src/util.hpp
+++ src/util.hpp
@@ -3,6 +3,9 @@
 
 #include <stdexcept>
 #include <err.h>
+#include <dirent.h>
+#include <vector> 
+#include <cstring>
 
 // exit() and err() breaks invariants for RAII (destructors). Therefore we use
 // warn() instead to printf an error and throw afterwards to exit.
@@ -10,3 +13,7 @@
   warn(__VA_ARGS__); \
   throw std::runtime_error("See error above"); \
   }
+
+
+std::string get_iommu_group(std::string pci_device);
+std::vector<int>  get_hardware_ids(std::string pci_device,std::string iommu_group);
