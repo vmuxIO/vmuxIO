accaee1125265f16be1d8e46d6e53eaf6d75477e
Pogobanane <aenderboy@gmx.de>
2023-07-18 09:12:06 +0000

    qemu-libvfio: update 7.0 -> 8.0
    this new version contains emulation support for i40 NICs while retaining libvfio-user support

--- nix/qemu-libvfio.nix
+++ nix/qemu-libvfio.nix
@@ -4,11 +4,11 @@ qemu_full.overrideAttrs ( new: old: {
   src = fetchFromGitHub {
     owner = "oracle";
     repo = "qemu";
-    rev = "b3b53245edbd399eb3ba1655d509478c76d37a8e";
-    hash = "sha256-kCX2ByuJxERLY2nHjPndVoo7TQm1j4qrpLjRcs42HU4=";
+    rev = "ed8ad9728a9c0eec34db9dff61dfa2f1dd625637"; # main 18.07.2023
+    hash = "sha256-kGNJ1yt44L6y/kw9YFJjbyQQ1/hzoI+tuun8p7ObnqM=";
     fetchSubmodules = true;
   };
-  version = "7.1.5";
+  version = "8.0.50";
   buildInputs = [ libndctl ] ++ old.buildInputs;
   nativeBuildInputs = [ json_c cmocka ] ++ old.nativeBuildInputs;
   configureFlags = old.configureFlags ++ [
