01f704fc8f133e3ca0c941126dc6b089c7628ac9
Pogobanane <aenderboy@gmx.de>
2023-08-04 14:58:52 +0000

    qemu builds and runs now

--- nix/qemu-libvfio.nix
+++ nix/qemu-libvfio.nix
@@ -9,23 +9,83 @@ qemu_full.overrideAttrs ( new: old: rec {
     hash = "sha256-kGNJ1yt44L6y/kw9YFJjbyQQ1/hzoI+tuun8p7ObnqM=";
     fetchSubmodules = true;
   };
+  
+  #when updating qemu, sync these with what is specified in qemu/subprojects/*.wrap
+  libvfio-user-src = pkgs.fetchFromGitLab {
+    owner = "qemu-project";
+    repo = "libvfio-user";
+    rev = "0b28d205572c80b568a1003db2c8f37ca333e4d7";
+    hash = "sha256-V05nnJbz8Us28N7nXvQYbj66LO4WbVBm6EO+sCjhhG8=";
+    fetchSubmodules = true;
+  };
+  dtc-src = pkgs.fetchFromGitLab {
+    owner = "qemu-project";
+    repo = "dtc";
+    rev = "b6910bec11614980a21e46fbccc35934b671bd81";
+    hash = "sha256-gx9LG3U9etWhPxm7Ox7rOu9X5272qGeHqZtOe68zFs4=";
+    fetchSubmodules = true;
+  };
+  keycodemapdb-src = pkgs.fetchFromGitLab {
+    owner = "qemu-project";
+    repo = "keycodemapdb";
+    rev = "f5772a62ec52591ff6870b7e8ef32482371f22c6";
+    hash = "sha256-EQrnBAXQhllbVCHpOsgREzYGncMUPEIoWFGnjo+hrH4=";
+    fetchSubmodules = true;
+  };
+  bsf3-src = pkgs.fetchFromGitLab {
+    owner = "qemu-project";
+    repo = "berkeley-softfloat-3";
+    rev = "b64af41c3276f97f0e181920400ee056b9c88037";
+    hash = "sha256-Yflpx+mjU8mD5biClNpdmon24EHg4aWBZszbOur5VEA=";
+    fetchSubmodules = true;
+  };
+  btf3-src = pkgs.fetchFromGitLab {
+    owner = "qemu-project";
+    repo = "berkeley-testfloat-3";
+    rev = "40619cbb3bf32872df8c53cc457039229428a263";
+    hash = "sha256-EBz1uYnjehCtJqrSFzERH23N5ELZU3gGM26JnsGFcWg=";
+    fetchSubmodules = true;
+  };
+
   version = "8.0.50";
   buildInputs = [ libndctl ] ++ old.buildInputs;
   nativeBuildInputs = [ json_c cmocka ] ++ old.nativeBuildInputs;
+  hardeningDisable = [ "all" ];
+
+  # emulate meson subproject dependency management
+  postUnpack = ''
+    cp -r ${libvfio-user-src} $sourceRoot/subprojects/libvfio-user
+    chmod -R u+w $sourceRoot/subprojects/libvfio-user
+
+    cp -r ${dtc-src} $sourceRoot/subprojects/dtc
+    chmod -R u+w $sourceRoot/subprojects/dtc
+
+    cp -r ${keycodemapdb-src} $sourceRoot/subprojects/keycodemapdb
+    chmod -R u+w $sourceRoot/subprojects/keycodemapdb
+
+    cp -r ${bsf3-src} $sourceRoot/subprojects/berkeley-softfloat-3
+    chmod -R u+w $sourceRoot/subprojects/berkeley-softfloat-3
+    cp $sourceRoot/subprojects/packagefiles/berkeley-softfloat-3/* $sourceRoot/subprojects/berkeley-softfloat-3
+
+    cp -r ${btf3-src} $sourceRoot/subprojects/berkeley-testfloat-3
+    chmod -R u+w $sourceRoot/subprojects/berkeley-testfloat-3
+    cp $sourceRoot/subprojects/packagefiles/berkeley-testfloat-3/* $sourceRoot/subprojects/berkeley-testfloat-3
+  '';
+
   configureFlags = # old.configureFlags ++ 
   [ # old.configureFlags
   "--disable-dependency-tracking"
-  "--prefix=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50"
-  "--bindir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/bin"
-  "--sbindir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/sbin"
-  "--includedir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/include"
-  "--oldincludedir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/include"
-  "--mandir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/share/man"
-  "--infodir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/share/info"
-  "--docdir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/share/doc/qemu"
-  "--libdir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/lib"
-  "--libexecdir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/libexec"
-  "--localedir=/nix/store/52x85sa3gcl481gxcbx27bgl22avimz4-qemu-8.0.50/share/locale"
+  # "--prefix=${out}"
+  # "--bindir=${out}/bin"
+  # "--sbindir=${out}/sbin"
+  # "--includedir=${out}/include"
+  # "--oldincludedir=${out}/include"
+  # "--mandir=${out}/share/man"
+  # "--infodir=${out}/share/info"
+  # "--docdir=${out}/share/doc/qemu"
+  # "--libdir=${out}/lib"
+  # "--libexecdir=${out}/libexec"
+  # "--localedir=${out}/share/locale"
   "--disable-strip"
   "--enable-docs"
   "--enable-tools"
@@ -93,7 +153,7 @@ qemu_full.overrideAttrs ( new: old: rec {
     "--disable-vvfat"
     "--disable-qed"
     "--disable-parallels"
-  ] ++ [ "--enable-vfio-user-server"];
+  ] ++ [ "--enable-kvm" "--enable-vfio-user-server"];
   patchPath = "${nixpkgs.outPath}/pkgs/applications/virtualization/qemu";
   patches = # old.patches ++ 
     [
@@ -105,4 +165,23 @@ qemu_full.overrideAttrs ( new: old: rec {
       ./0001-qemu-hva2gpa.patch
       ./0001-qemu-dma_read.patch
     ];
+
+  # preInstall = ''
+  #   pwd
+  #   ls
+  #   echo $sourceRoot
+  #   echo "stub" > qemu-kvm
+  # '';
+  preInstall = ''
+    echo foobar $out
+    mkdir -p $out
+  '';
+  postInstall = ''
+    echo foobar
+    pwd
+    ls
+    echo $out
+    mkdir -p $out/bin
+    ls $out
+  '';
 })
