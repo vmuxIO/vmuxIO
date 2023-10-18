391311bbb82c7a75975d4a3a8a6030b472fe3c28
Pogobanane <aenderboy@gmx.de>
2023-10-18 21:46:48 +0000

    e1000-emu: connect large part of APIs

--- src/devices/e1000.hpp
+++ src/devices/e1000.hpp
@@ -1,27 +1,35 @@
 #include "device.hpp"
+#include "nic-emu.hpp"
+
+static bool rust_logs_initialized = false;
 
 class E1000EmulatedDevice : public VmuxDevice {
   private:
-    std::unique_ptr<i40e::i40e_bm> model; // TODO rename i40e_bm class to e810
-                                          //
-    /** we don't pass the entire device to the model, but only the callback
-     * adaptor. We choose this approach, because we don't want to purpose
-     * build the device to fit the simbricks code, and don't want to change
-     * the simbricks code too much to fit the vmux code. **/
-    std::shared_ptr<nicbm::CallbackAdaptor> callbacks;
-
-    SimbricksProtoPcieDevIntro deviceIntro = SimbricksProtoPcieDevIntro();
+    E1000FFI* e1000;
+    static const int bars_nr = 2;
 
   public:
     E1000EmulatedDevice() {
-      // printf("foobar %zu\n", nicbm::kMaxDmaLen);
-      // i40e::i40e_bm* model = new i40e::i40e_bm();
-      this->model = std::make_unique<i40e::i40e_bm>();
+      if (!rust_logs_initialized) {
+          initialize_rust_logging(4); // Debug logs
+          rust_logs_initialized = true;
+      }
 
-      this->callbacks = std::make_shared<nicbm::CallbackAdaptor>();
+      auto callbacks = FfiCallbacks {
+          this,
+          send_cb,
+          dma_read_cb,
+          dma_write_cb,
+          issue_interrupt_cb,
+      };
 
-      this->model->vmux = this->callbacks;
+      e1000 = new_e1000(callbacks);
+
       this->init_pci_ids();
+    }
+
+    ~E1000EmulatedDevice() {
+        drop_e1000(e1000);
     }
 
     void setup_vfu(VfioUserServer &vfu) {
@@ -37,22 +45,35 @@ class E1000EmulatedDevice : public VmuxDevice {
     };
 
     void init_pci_ids() {
-      this->model->SetupIntro(this->deviceIntro);
-      this->info.pci_vendor_id = this->deviceIntro.pci_vendor_id;
-      this->info.pci_device_id = this->deviceIntro.pci_device_id;
+      this->info.pci_vendor_id = 0x8086;
+      this->info.pci_device_id = 0x100e;
       // Some values are not set by SetupIntro
       this->info.pci_subsystem_vendor_id = 0x0086;
       this->info.pci_subsystem_id = 0x0001;
       this->info.pci_device_revision_id = 0x2;
-      this->info.pci_class = this->deviceIntro.pci_class;
-      this->info.pci_subclass = this->deviceIntro.pci_subclass;
-      this->info.pci_revision = this->deviceIntro.pci_revision;
+      this->info.pci_class = 0x02;
+      this->info.pci_subclass = 0x00;
+      this->info.pci_revision = 0x3;
       __builtin_dump_struct(&this->info, &printf);
-      __builtin_dump_struct(&this->deviceIntro, &printf);
-      this->model->SetupIntro(this->deviceIntro);
     }
 
   private:
+    static void send_cb(void *private_ptr, const uint8_t *buffer, uintptr_t len) {
+        die("Send CB\n"); // TODO connect this and the ones below
+    }
+
+    static void dma_read_cb(void *private_ptr, uintptr_t dma_address, uint8_t *buffer, uintptr_t len) {
+        die("Dma read CB\n");
+    }
+
+    static void dma_write_cb(void *private_ptr, uintptr_t dma_address, const uint8_t *buffer, uintptr_t len) {
+        die("Dma write CB\n");
+    }
+
+    static void issue_interrupt_cb(void *private_ptr) {
+        die("Issue interrupt CB\n");
+    }
+
     void init_general_callbacks(VfioUserServer &vfu) {
       // TODO all those callback functions need implementation
       int ret;
@@ -124,45 +145,39 @@ class E1000EmulatedDevice : public VmuxDevice {
     }
 
     void init_bar_callbacks(VfioUserServer &vfu) {
-      for (int idx = 0; idx < SIMBRICKS_PROTO_PCIE_NBARS; idx++) {
-        auto region = this->deviceIntro.bars[idx];
+      int ret;
 
-        int ret;
-
-        if (region.len == 0) {
-            printf("Bar region %d skipped.\n", idx);
-        }
-
-        // set up register accesses VM<->vmux
-        
-        int flags = convert_flags(region.flags);
-        flags |= VFU_REGION_FLAG_RW;
-        ret = vfu_setup_region(vfu.vfu_ctx, idx,
-                region.len,
-                &(this->expected_access_callback),
-                flags, NULL, 
-                0, // nr. items in bar_mmap_areas
-                -1, 0); // fd -1 and offset 0 because fd is unused
-        if (ret < 0) {
-            die("failed to setup BAR region %d", idx);
-        }
-
-        // init some flags that are also set with qemu passthrough
-        vfu_pci_config_space_t *config_space =
-            vfu_pci_get_config_space(vfu.vfu_ctx);
-        vfu_bar_t *bar_config = &(config_space->hdr.bars[idx]);
-        // see pci spec sec 7.5.1.2.1 for meaning of bits:
-        if (region.flags & SIMBRICKS_PROTO_PCIE_BAR_PF) {
-          bar_config->mem.prefetchable = 1; // prefetchable
-        }
-        if (region.flags & SIMBRICKS_PROTO_PCIE_BAR_64) {
-          bar_config->mem.locatable = 0b10; // 64 bit
-        }
-
-        printf("Vfio-user: Bar region %d \
-                (size 0x%x) set up.\n", idx,
-                (uint)region.len);
+      // set up register accesses VM<->vmux
+      int bar_idx = 0;
+      int bar_len = 0x20000;
+      
+      int flags = VFU_REGION_FLAG_MEM;
+      flags |= VFU_REGION_FLAG_RW;
+      ret = vfu_setup_region(vfu.vfu_ctx, bar_idx,
+              bar_len,
+              &(this->expected_access_callback),
+              flags, NULL, 
+              0, // nr. items in bar_mmap_areas
+              -1, 0); // fd -1 and offset 0 because fd is unused
+      if (ret < 0) {
+          die("failed to setup BAR region %d", bar_idx);
       }
+
+      // // init some flags that are also set with qemu passthrough
+      // vfu_pci_config_space_t *config_space =
+      //     vfu_pci_get_config_space(vfu.vfu_ctx);
+      // vfu_bar_t *bar_config = &(config_space->hdr.bars[bar_idx]);
+      // // see pci spec sec 7.5.1.2.1 for meaning of bits:
+      // if (region.flags & SIMBRICKS_PROTO_PCIE_BAR_PF) {
+      //   bar_config->mem.prefetchable = 1; // prefetchable
+      // }
+      // if (region.flags & SIMBRICKS_PROTO_PCIE_BAR_64) {
+      //   bar_config->mem.locatable = 0b10; // 64 bit
+      // }
+
+      printf("Vfio-user: Bar region %d \
+              (size 0x%x) set up.\n", bar_idx,
+              (uint)bar_len);
     }
     static ssize_t expected_access_callback(
               [[maybe_unused]] vfu_ctx_t *vfu_ctx,
@@ -172,9 +187,12 @@ class E1000EmulatedDevice : public VmuxDevice {
               [[maybe_unused]] const bool is_write
               )
       {
-        printf("a vfio register/DMA access callback was \
-                triggered (at 0x%lx, is write %d.\n",
+        E1000EmulatedDevice *device = (E1000EmulatedDevice*) vfu_get_private(vfu_ctx);
+        printf("a vfio register/DMA access callback was triggered (at 0x%lx, is write %d).\n",
                 offset, is_write);
+        if (e1000_region_access(device->e1000, 0, offset, (uint8_t*) buf, count, is_write)) { // TODO fixed bar number
+          return count;
+        }
         return 0;
       }
 };
