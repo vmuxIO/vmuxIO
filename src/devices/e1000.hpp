f1e9739b703d358b793bb45f30d1d98751a14702
Pogobanane <aenderboy@gmx.de>
2023-12-12 10:22:31 +0000

    vmux-e1000/throttling: superclass

--- src/devices/e1000.hpp
+++ src/devices/e1000.hpp
@@ -15,11 +15,19 @@ static bool rust_logs_initialized = false;
 
 class E1000EmulatedDevice;
 
+class InterruptThrottler {
+  public:
+  std::shared_ptr<VfioUserServer> vfuServer;
+
+  // InterruptThrottler(int efd, int irq_idx) {};
+  virtual ulong try_interrupt(ulong interrupt_spacing, bool int_pending) = 0;
+};
+
 /*
  * Does many things, but the "physical" limit of e1000 of ~8000irq/s is enforced by behavioral model
  */
-class InterruptThrottler {
-  public:
+class InterruptThrottlerQemu: public InterruptThrottler {
+  private:
   struct timespec last_interrupt_ts = {};
   // ulong interrupt_spacing = 250 * 1000; // nsec
   std::atomic<bool> is_deferred = false;
@@ -27,19 +35,20 @@ class InterruptThrottler {
   int efd;
   int irq_idx;
   epoll_callback timer_callback;
-  std::shared_ptr<VfioUserServer> vfuServer;
   ulong factor = 10;
   bool last_pkt_irqed = false;
 
-  InterruptThrottler(int efd, int irq_idx): irq_idx(irq_idx) {
+  public: 
+  InterruptThrottlerQemu(int efd, int irq_idx): irq_idx(irq_idx) {
     this->timer_fd = timerfd_create(CLOCK_MONOTONIC, 0); // foo error
     this->registerEpoll(efd);
 
   }
 
+  private:
   void registerEpoll(int efd) {
     this->timer_callback.fd = this->timer_fd;
-    this->timer_callback.callback = InterruptThrottler::timer_cb;
+    this->timer_callback.callback = InterruptThrottlerQemu::timer_cb;
     this->timer_callback.ctx = this;
     struct epoll_event e;
     e.events = EPOLLIN;
@@ -52,7 +61,7 @@ class InterruptThrottler {
   }
 
   static void timer_cb(int fd, void* this__) {
-    InterruptThrottler* this_ = (InterruptThrottler*) this__;
+    InterruptThrottlerQemu* this_ = (InterruptThrottlerQemu*) this__;
     this_->send_interrupt();
     struct itimerspec its = {};
     timerfd_settime(this_->timer_fd, 0, &its, NULL); // foo error
@@ -68,6 +77,7 @@ class InterruptThrottler {
     timerfd_settime(this->timer_fd, 0, &its, NULL); // foo error
   }
 
+  public: 
   /* interrupt_spacing in ns
    *
    * Qemu to my understanding defers interrupts by min(ITR, 111us) if an interrupt is still pending but last rx no interrupt was pending. It skips the interrupt, if another interrupt is already delayed. 
@@ -114,6 +124,7 @@ class InterruptThrottler {
     return we_defer;
   }
 
+  private:
   __attribute__((noinline)) void send_interrupt() {
     int ret = vfu_irq_trigger(this->vfuServer->vfu_ctx, this->irq_idx);
     if_log_level(LOG_DEBUG, printf("Triggered interrupt. ret = %d, errno: %d\n", ret, errno));
@@ -127,7 +138,7 @@ class E1000EmulatedDevice : public VmuxDevice {
 private:
   E1000FFI *e1000;
   std::shared_ptr<Tap> tap;
-  InterruptThrottler irqThrottle;
+  InterruptThrottlerQemu irqThrottle;
   static const int bars_nr = 2;
   static const int IRQ_IDX = 0; // this emulator may register multiple interrupts, but only uses the first one
   epoll_callback tapCallback;
