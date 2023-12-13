3af05b244a95c5791f3ede0a1141bc0d96e448ae
Pogobanane <aenderboy@gmx.de>
2023-12-13 09:19:16 +0000

    kernel irq rates of max. core

--- subprojects/irq-rates.py
+++ subprojects/irq-rates.py
@@ -1,6 +1,10 @@
 import time
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("topic", help="The topic to filter for")
+args = parser.parse_args()
 
-topic = "ice"
+topic = args.topic
 
 def deduplicate_whitespace(s):
     return " ".join(s.split())
@@ -24,6 +28,7 @@ def filter_topic(table, topic):
             continue
         if topic in row[-1] or topic in row[-2] or topic in row[-3]:
             ret += [row]
+    if len(ret) == 0: print("WARNING: nothing passed the filter")
     return ret 
 
 def count_irqs(table): 
@@ -33,18 +38,37 @@ def count_irqs(table):
             count += int(row[i])
     return count
 
-def irq_count():
-    table = tabularize("/proc/interrupts")
-    table = filter_topic(table, topic)
-    return count_irqs(table)
+def count_irqs_per_core(table):
+    count = [0 for i in range(1, len(table[0]) - 2)]
+    for row in table:
+        for i in range(1, len(row) - 3):
+            count[i] += int(row[i])
+    return count
+
+def diff(a, b):
+    ret = []
+    for i in range(0, len(a)):
+        ret += [a[i] - b[i]]
+    return ret
 
 def irq_rate():
-    old_count = irq_count()
+    table = tabularize("/proc/interrupts")
+    table = filter_topic(table, topic)
+
+    old_count = count_irqs(table)
+    old_counts = count_irqs_per_core(table)
     while(True):
-        new_count = irq_count()
+        table = tabularize("/proc/interrupts")
+        table = filter_topic(table, topic)
+
+        new_count = count_irqs(table)
         count = new_count - old_count
         old_count = new_count
-        print(count, flush=True)
+
+        new_counts = count_irqs_per_core(table)
+        counts = diff(new_counts, old_counts)
+        old_counts = new_counts
+        print(f"irq/s: {count}\t max/core: {max(counts)}", flush=True)
         time.sleep(1)
 
         
