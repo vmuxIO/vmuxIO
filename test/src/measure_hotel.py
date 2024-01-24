04e3de6b32b2ef531afd670c34e468ca58391493
Pogobanane <aenderboy@gmx.de>
2024-01-24 17:04:09 +0000

    measure_hotel: split docker-compose and start first service in VM

--- test/src/measure_hotel.py
+++ test/src/measure_hotel.py
@@ -11,6 +11,8 @@ from measure import Measurement
 from util import safe_cast
 from typing import Iterator, cast
 import time
+import yaml
+from root import *
 
 OUT_DIR = "/tmp/out1"
 
@@ -30,71 +32,97 @@ def main() -> None:
     loadgen.setup_hugetlbfs()
 
 
-    interface = Interface.VMUX_PT
-    # TODO put early skip check here
+    # interface = Interface.VMUX_PT
+    # # TODO put early skip check here
+    # with measurement.virtual_machine(interface) as guest:
+    #
+    #     # guest: set up interfaces and networking
+    #
+    #     debug("Detecting guest test interface")
+    #     guest.detect_test_iface()
+    #     guest.bind_test_iface()
+    #     guest.setup_hugetlbfs()
+    #
+    #     # the actual test
+    #
+    #     loadgen.stop_moongen_reflector()
+    #     loadgen.start_moongen_reflector()
+    #
+    #     test = LoadLatencyTest(
+    #         machine=Machine.PCVM,
+    #         interface=interface,
+    #         mac=loadgen.test_iface_mac,
+    #         qemu="measure-vnf", # abuse this as comment field
+    #         vhost=False,
+    #         ioregionfd=False,
+    #         reflector=Reflector.MOONGEN,
+    #         rate=1,
+    #         size=64,
+    #         runtime=30,
+    #         repetitions=1,
+    #         warmup=False,
+    #         cooldown=False,
+    #         outputdir=OUT_DIR,
+    #     )
+    #     guest_: LoadGen = cast(LoadGen, guest) # trust me bro, this works
+    #     test.run(guest_)
+    #
+    #     loadgen.stop_moongen_reflector()
+
+    # create per-VM docker-compose
+    with open(f"{PROJECT_ROOT}/subprojects/deathstarbench/hotelReservation/docker-compose.yml", "r") as file:
+        docker_compose_full = yaml.safe_load(file)
+    version = docker_compose_full["version"]
+    docker_compose = dict()
+    docker_compose["version"] = f"version"
+    docker_compose["services"] = dict()
+    docker_compose["services"]["consul"] = docker_compose_full["services"]["consul"]
+    docker_compose["services"]["consul"]["network_mode"] = "host" # --network host
+    docker_compose_str = yaml.dump(docker_compose)
+    docker_image = docker_compose["services"]["consul"]["image"]
+
+    docker_images = f"{measurement.config['guest']['moonprogs_dir']}/../../VMs/docker-images.tar"
+
+    interface = Interface.BRIDGE
+
     with measurement.virtual_machine(interface) as guest:
+        try:
+            # guest: set up interfaces and networking
 
-        # guest: set up interfaces and networking
+            # debug("Detecting guest test interface")
+            # interface is already bound and up (without ip)
 
-        debug("Detecting guest test interface")
-        guest.detect_test_iface()
-        guest.bind_test_iface()
-        guest.setup_hugetlbfs()
 
-        # the actual test
+            # the actual test
 
-        loadgen.stop_moongen_reflector()
-        loadgen.start_moongen_reflector()
+            guest.exec(f'echo "{docker_compose_str}" > docker-compose.yaml')
+            guest.exec(f"docker load -i {docker_images}")
+            guest.exec(f"docker-compose up -d")
 
-        test = LoadLatencyTest(
-            machine=Machine.PCVM,
-            interface=interface,
-            mac=loadgen.test_iface_mac,
-            qemu="measure-vnf", # abuse this as comment field
-            vhost=False,
-            ioregionfd=False,
-            reflector=Reflector.MOONGEN,
-            rate=1,
-            size=64,
-            runtime=30,
-            repetitions=1,
-            warmup=False,
-            cooldown=False,
-            outputdir=OUT_DIR,
-        )
-        guest_: LoadGen = cast(LoadGen, guest) # trust me bro, this works
-        test.run(guest_)
+            # TODO start more services and so on
 
-        loadgen.stop_moongen_reflector()
+            breakpoint()
+            guest.exec(f"docker-compose down")
+            breakpoint()
 
-    interface = Interface.VFIO
+            loadgen.stop_moongen_reflector()
+            loadgen.start_moongen_reflector()
 
-    with measurement.virtual_machine(interface) as guest:
-        # guest: set up interfaces and networking
+            # TODO check if we can skip
+            time.sleep(1) # give ip link time to come up
+            info("Running kni-latency")
+            remote_output_file = "/tmp/out.log"
+            guest.exec(
+                    f"{measurement.config['guest']['moonprogs_dir']}/../kni-latency/kni-latency "
+                    f"{loadgen.test_iface_mac} {guest.test_iface} "
+                    f"| tee {remote_output_file}"
+                    )
+            guest.copy_from(remote_output_file,
+                        f"{OUT_DIR}/measure_vnf_rep0.log")
 
-        debug("Detecting guest test interface")
-        guest.exec("sudo modprobe ice")
-        guest.bind_device(guest.test_iface_addr, "ice")
-        guest.exec(f"sudo ip link set {guest.test_iface} up")
-
-        # the actual test
-
-        loadgen.stop_moongen_reflector()
-        loadgen.start_moongen_reflector()
-
-        # TODO check if we can skip
-        time.sleep(1) # give ip link time to come up
-        info("Running kni-latency")
-        remote_output_file = "/tmp/out.log"
-        guest.exec(
-                f"{measurement.config['guest']['moonprogs_dir']}/../kni-latency/kni-latency "
-                f"{loadgen.test_iface_mac} {guest.test_iface} "
-                f"| tee {remote_output_file}"
-                )
-        guest.copy_from(remote_output_file,
-                    f"{OUT_DIR}/measure_vnf_rep0.log")
-
-        loadgen.stop_moongen_reflector()
+            loadgen.stop_moongen_reflector()
+        except Exception:
+            breakpoint()
 
 
 if __name__ == "__main__":
