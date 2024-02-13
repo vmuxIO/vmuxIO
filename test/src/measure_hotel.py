54142f61d7698864d87abbfa4e366ab1a7440a8a
Pogobanane <aenderboy@gmx.de>
2024-02-13 17:39:21 +0000

    measure_hotel: runtime estimation

--- test/src/measure_hotel.py
+++ test/src/measure_hotel.py
@@ -20,6 +20,9 @@ import subprocess
 
 OUT_DIR: str
 BRIEF: bool
+DURATION_S: int
+
+# TODO deduplicate code via abstrations from measure.py
 
 @dataclass
 class DeathStarBenchTest:
@@ -45,6 +48,61 @@ class DeathStarBenchTest:
                 return True
         return False
 
+    def estimated_boottime(self) -> float:
+        """
+        estimated time to boot this system in seconds
+        """
+        vm_boot = (self.num_vms / 32) * 60 * 2 # time taken to boot a batch of VMs
+        return vm_boot
+
+    def estimated_runtime(self) -> float:
+        """
+        estimate time needed to run this benchmark excluding boottime in seconds
+        """
+        app_setup = 0 # diff "running DeathStarBenchTest" "Running wrk2 x times"
+        measurements = self.repetitions * DURATION_S
+        if self.app == "hotelReservation":
+            app_setup = (self.num_vms / 19) * 60 * 1
+        if self.app == "socialNetwork":
+            app_setup = (self.num_vms / 27) * 60 * 1.5
+        if self.app == "mediaMicroservices":
+            app_setup = (self.num_vms / 27) * 60 * 2 # TODO
+
+        return measurements + app_setup
+
+    @staticmethod
+    def estimate_time(test_matrix: Dict[str, List[Any]], args_reboot: List[str]) -> float:
+        """
+        Test matrix: like kwargs used to initialize DeathStarBenchTest, but every value is the list of values.
+        kwargs_reboot: test_matrix keys. Changing these parameters requires a reboot.
+        """
+        total = 0
+        needed = 0
+        needed_s = 0.0
+        for test_args in product_dict(test_matrix):
+            test = DeathStarBenchTest(**test_args)
+            total += 1
+            if test.needed():
+                needed += 1
+                needed_s += test.estimated_runtime()
+
+        # args not relevant for reboots
+        args_no_reboot = [ key for key in test_matrix.keys() if key not in args_reboot ]
+        # example (1st) test parameters not relevant for reboots
+        kwargs_no_reboot = {key: test_matrix[key][0] for key in args_no_reboot }
+        # test parameters relevant for reboots:
+        reboot_matrix = {key: value for key, value in test_matrix.items() if key in args_reboot}
+        # loop over all reboots
+        for test_args in product_dict(reboot_matrix):
+            test_args = {**test_args, **kwargs_no_reboot}
+            test = DeathStarBenchTest(**test_args)
+            needed_s += test.estimated_boottime()
+
+        info(f"Test not cached yet: {needed}/{total}. Expected time to completion: {needed_s/60:.0f}min ({needed_s/60/60:.1f}h)")
+
+        return needed_s
+
+
     @staticmethod
     def any_needed(test_matrix: Dict[str, List[Any]]) -> bool:
         """
@@ -62,8 +120,8 @@ class DeathStarBench:
 
     app: str
     docker_image_tar: str
-    docker_compose: Dict[int, str] = {} # vm id -> strings of content of docker-compose.yamls    
-    service_map: Dict[int, str] = {} # vm id -> service name
+    docker_compose: Dict[int, str] # vm id -> strings of content of docker-compose.yamls    
+    service_map: Dict[int, str] # vm id -> service name
     extra_hosts: str = "" # /etc/hosts format
     frontend: str # service name of frontend
     frontend_url: str = "host:port/api"
@@ -79,6 +137,8 @@ class DeathStarBench:
         self.app = app
         self.docker_image_tar = f"{moonprogs_dir}/../../VMs/docker-images-{self.app}.tar"
 
+        self.docker_compose = {}
+        self.service_map = {}
         self.parse_docker_compose()
         
         if self.app == "hotelReservation":
@@ -129,6 +189,7 @@ class DeathStarBench:
             docker_compose["services"][service] = docker_compose_full["services"][service]
             docker_compose["services"][service]["network_mode"] = "host" # --network host
             docker_compose["services"][service]["depends_on"] = dict() # ignore stated dependencies (cant be resolved because they are not local)
+            docker_compose["services"][service]["restart"] = "on-failure"
             if self.app == "hotelReservation":
                 docker_compose["services"][service]["volumes"] = [ "./config.json:/config.json" ]
 
@@ -159,7 +220,7 @@ class DeathStarBench:
         return str(json.dumps(config))
 
     def docker_compose_cleanup(self, guest: Guest):
-        guest.exec("docker compose down || true")
+        guest.exec("docker-compose down || true")
         # maybe also docker compose rm and docker volume rm
 
     def install_configs(self, guest: Guest):
@@ -229,14 +290,13 @@ class DeathStarBench:
         info(f"Running wrk2 {test.repetitions} times")
 
         remote_output_file = "/tmp/output.log"
-        duration_s = 61 if not BRIEF else 11
         for repetition in range(test.repetitions):
             local_output_file = test.output_filepath(repetition)
             loadgen.exec(f"sudo rm {remote_output_file} || true")
             LoadGen.stop_wrk2(loadgen)
             workdir = f"{loadgen.moonprogs_dir}/../../subprojects/deathstarbench/{self.app}"
-            LoadGen.start_wrk2(loadgen, self.frontend_url, self.script, duration=duration_s, outfile=remote_output_file, workdir=workdir)
-            time.sleep(duration_s + 1)
+            LoadGen.start_wrk2(loadgen, self.frontend_url, self.script, duration=DURATION_S, outfile=remote_output_file, workdir=workdir)
+            time.sleep(DURATION_S + 1)
             try:
                 loadgen.wait_for_success(f'[[ $(tail -n 1 {remote_output_file}) = *"AUTOTEST_DONE"* ]]')
             except TimeoutError:
@@ -260,6 +320,7 @@ def main() -> None:
     from measure import OUT_DIR as M_OUT_DIR, BRIEF as M_BRIEF
     global OUT_DIR
     global BRIEF
+    global DURATION_S
     OUT_DIR = M_OUT_DIR
     BRIEF = M_BRIEF
 
@@ -267,6 +328,7 @@ def main() -> None:
     rpsList = [ 10, 100, 200, 300, 400, 500, 600 ]
     apps = [ "hotelReservation", "socialNetwork", "mediaMicroservices" ]
     repetitions = 4
+    DURATION_S = 61 if not BRIEF else 11
     if BRIEF:
         interfaces = [ Interface.BRIDGE_E1000 ]
         # interfaces = [ Interface.VMUX_EMU ]
@@ -276,25 +338,38 @@ def main() -> None:
         apps = [ "mediaMicroservices" ]
         repetitions = 1
 
-
+    # plan the measurement
+    moonprogs_dir = f"{measurement.config['guest']['moonprogs_dir']}"
+    deathstarbenches = {app: DeathStarBench(app, moonprogs_dir) for app in apps}
     for app in apps:
-        
-        moonprogs_dir = f"{measurement.config['guest']['moonprogs_dir']}"
-        deathstar = DeathStarBench(app, moonprogs_dir)
-        num_vms = len(deathstar.docker_compose)
-
+        bench = deathstarbenches[app]
         test_matrix = dict(
             interface=[ interface.value for interface in interfaces],
             rps=rpsList,
             app=[ app ],
             repetitions=[ repetitions ],
-            num_vms=[ num_vms ]
+            num_vms=[ len(bench.docker_compose) ]
             )
-        if not DeathStarBenchTest.any_needed(test_matrix):
-            warning(f"Skipping app {app}: All measurements done already.")
-            continue
+        info(f"Execution plan {app}:")
+        DeathStarBenchTest.estimate_time(test_matrix, ["app", "interface", "num_vms"])
+
+    for app in apps:
+        
+        deathstar = deathstarbenches[app]
+        num_vms = len(deathstar.docker_compose)
 
         for interface in interfaces:
+            test_matrix = dict(
+                interface=[ interface.value ],
+                rps=rpsList,
+                app=[ app ],
+                repetitions=[ repetitions ],
+                num_vms=[ num_vms ]
+                )
+            if not DeathStarBenchTest.any_needed(test_matrix):
+                warning(f"Skipping app {app} with {interface.value}: All measurements done already.")
+                continue
+
             with measurement.virtual_machines(interface, num=num_vms) as guests:
                 try:
                     # loadgen: set up interfaces and networking
