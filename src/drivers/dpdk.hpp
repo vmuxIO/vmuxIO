793803f2dc24b3ffe783b9ae0c0c28f966882727
Pogobanane <aenderboy@gmx.de>
2024-03-07 13:19:12 +0000

    vmuxDpdk: dont assert link status
    vmux doesnt care if the like is up or down

--- src/drivers/dpdk.hpp
+++ src/drivers/dpdk.hpp
@@ -66,31 +66,6 @@ copy_buf_to_pkt(void *buf, unsigned len, struct rte_mbuf *pkt, unsigned offset)
 	copy_buf_to_pkt_segs(buf, len, pkt, offset);
 }
 
-#define CHECK_INTERVAL 1000  /* 100ms */
-#define MAX_REPEAT_TIMES 90  /* 9s (90 * 100ms) in total */
-
-static void
-assert_link_status(uint16_t port_id)
-{
-	struct rte_eth_link link;
-	uint8_t rep_cnt = MAX_REPEAT_TIMES;
-	int link_get_err = -EINVAL;
-
-	memset(&link, 0, sizeof(link));
-	do {
-		link_get_err = rte_eth_link_get(port_id, &link);
-		if (link_get_err == 0 && link.link_status == RTE_ETH_LINK_UP)
-			break;
-		rte_delay_ms(CHECK_INTERVAL);
-	} while (--rep_cnt);
-
-	if (link_get_err < 0)
-		rte_exit(EXIT_FAILURE, ":: error: link get is failing: %s\n",
-			 rte_strerror(-link_get_err));
-	if (link.link_status == RTE_ETH_LINK_DOWN)
-		rte_exit(EXIT_FAILURE, ":: error: link is still down\n");
-}
-
 /* Port initialization used in flow filtering. 8< */
 static void
 filtering_init_port(uint16_t port_id, uint16_t nr_queues, struct rte_mempool *mbuf_pool)
@@ -177,8 +152,6 @@ filtering_init_port(uint16_t port_id, uint16_t nr_queues, struct rte_mempool *mb
 			ret, port_id);
 	}
 	/* >8 End of starting the port. */
-
-	assert_link_status(port_id);
 
 	printf(":: initializing port: %d done\n", port_id);
 }
