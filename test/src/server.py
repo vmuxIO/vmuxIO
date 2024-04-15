c769a9997ee6ac35e79e637d39a348a933ebf2a0
HendrikHuebner <hendrik.huebner18@gmail.com>
2024-04-15 12:01:54 +0000

    fixed #124

--- test/src/server.py
+++ test/src/server.py
@@ -1622,15 +1622,18 @@ class Host(Server):
         # Build misc parameters
 
         dev_type = 'pci' if machine_type == 'pc' else 'device'
+
         # TODO we need a different qemu build dir for vmux
         qemu_bin_path = 'qemu-system-x86_64'
         if qemu_build_dir:
             qemu_bin_path = path_join(qemu_build_dir, qemu_bin_path)
+
         cpus = vcpus if vcpus else self.guest_vcpus
         mem = memory if memory else self.guest_memory
         disk_path = self.guest_root_disk_path
         if root_disk:
             disk_path = root_disk
+
         disk_path = MultiHost.disk_path(disk_path, vm_number)
         fsdev_config = ''
         if self.fsdevs:
@@ -1695,8 +1698,9 @@ class Host(Server):
         else:
             memory_path = f'/dev/shm/qemu-{vm_number}'
         # if path_getsize(memory_path) != mem * 1024 * 1024:
-        if self.test("[[ -f {memory_path} && $(stat --printf='%s' {memory_path}) -eq {mem*1024*1024} ]]"):
+        if self.test(f"[[ -f {memory_path} && $(stat --printf='%s' {memory_path}) -eq {mem*1024*1024} ]]"):
             self.exec(f"sudo rm {memory_path}")
+
         memory_backend = f' -object memory-backend-file,mem-path={memory_path},prealloc=yes,id=bm,size={mem}M,share=on'
 
         # Actually start qemu in tmux
