122a09cfaad5ba092647caa1445d0ce4e1e526a0
Pogobanane <aenderboy@gmx.de>
2024-05-16 19:45:58 +0000

    e810 adminq: add logger for add switch rule

--- src/sims/nic/e810_bm/e810_adminq.cc
+++ src/sims/nic/e810_bm/e810_adminq.cc
@@ -689,6 +689,49 @@ void queue_admin_tx::admin_desc_ctx::process() {
     struct ice_aqc_sw_rules *add_sw_rules_cmd = reinterpret_cast<ice_aqc_sw_rules *> (d->params.raw);
     struct ice_aqc_sw_rules_elem *add_sw_rules = reinterpret_cast<ice_aqc_sw_rules_elem*>(data);
     cout <<  "  set switch config" << logger::endl;
+
+    if (add_sw_rules->type == ICE_AQC_SW_RULES_T_LKUP_RX || add_sw_rules->type == ICE_AQC_SW_RULES_T_LKUP_TX) {
+      bool is_rx = add_sw_rules->type == ICE_AQC_SW_RULES_T_LKUP_RX;
+      cout << "   " << 
+        (is_rx ? "rx" : "tx") <<
+        " lookup recipe_id=" << add_sw_rules->pdata.lkup_tx_rx.recipe_id <<
+        (is_rx ? " src_port=" : " src_vsi=") << add_sw_rules->pdata.lkup_tx_rx.src << 
+        " rule_index=" << add_sw_rules->pdata.lkup_tx_rx.index <<
+        logger::endl;
+      cout << "   " << 
+        "hdr_len=" << add_sw_rules->pdata.lkup_tx_rx.hdr_len << 
+        logger::endl;
+      uint32_t action_type = (add_sw_rules->pdata.lkup_tx_rx.act & ICE_SINGLE_ACT_TYPE_M) >> ICE_SINGLE_ACT_TYPE_S;
+      uint32_t vsi_id_list = (add_sw_rules->pdata.lkup_tx_rx.act & ICE_SINGLE_ACT_VSI_ID_M) >> ICE_SINGLE_ACT_VSI_ID_S;
+      uint32_t queue_id = (add_sw_rules->pdata.lkup_tx_rx.act & ICE_SINGLE_ACT_Q_INDEX_M) >> ICE_SINGLE_ACT_Q_INDEX_S;
+      bool large_action = (add_sw_rules->pdata.lkup_tx_rx.act & ICE_SINGLE_ACT_PTR_BIT);
+      if (action_type == ICE_SINGLE_ACT_VSI_FORWARDING) {
+        cout << "   action VSI id/list=" << vsi_id_list <<
+          logger::endl;
+      } else if (action_type == ICE_SINGLE_ACT_TO_Q) {
+        cout << "   action Queue idx=" << queue_id << 
+          " region=?" << logger::endl;
+      } else if (action_type == ICE_SINGLE_ACT_PRUNE && !large_action) {
+        cout << "   action Prune vsi/list=?" << logger::endl;
+      } else if (action_type == ICE_SINGLE_ACT_PTR && large_action) {
+        cout << "   action Pointer (large action) ?=?" << logger::endl;
+      } else if (action_type == ICE_SINGLE_ACT_OTHER_ACTS) {
+        cout << "   action other ?=?" << logger::endl;
+      }
+    } else if (add_sw_rules->type == ICE_AQC_SW_RULES_T_LG_ACT) {
+      cout << "   large action" << logger::endl;
+    } else if (add_sw_rules->type == ICE_AQC_SW_RULES_T_VSI_LIST_SET) {
+      cout << "   vsi list set" << logger::endl;
+    } else if (add_sw_rules->type == ICE_AQC_SW_RULES_T_VSI_LIST_CLEAR) {
+      cout << "   vsi list clear" << logger::endl;
+    } else if (add_sw_rules->type == ICE_AQC_SW_RULES_T_PRUNE_LIST_SET) {
+      cout << "   vsi prune list set" << logger::endl;
+    } else if (add_sw_rules->type == ICE_AQC_SW_RULES_T_PRUNE_LIST_CLEAR) {
+      cout << "   vsi prune list clear" << logger::endl;
+    }
+
+    __builtin_dump_struct(add_sw_rules, &printf);
+
     add_sw_rules->type = ICE_AQC_SW_RULES_T_LKUP_TX;
     add_sw_rules->pdata.lkup_tx_rx.src = 1;
     add_sw_rules->pdata.lkup_tx_rx.index = 0;
@@ -699,6 +742,7 @@ void queue_admin_tx::admin_desc_ctx::process() {
     uint32_t offset = 0;
     offset |= nvm_read_cmd->offset_low;
     offset |= (nvm_read_cmd->offset_high) << 16;
+    printf("nvm read at 0x%x\n", offset);
 
     // copy responses observed on real hardware.
     if (offset == 0x0) { // some control register defining nvm layout
@@ -746,10 +790,10 @@ void queue_admin_tx::admin_desc_ctx::process() {
     // desc_complete(0);
   }
   else {
-    cout <<  "  uknown opcode=" << d->opcode << logger::endl;
+    cout <<  "  unknown opcode=0x" << std::hex << d->opcode << logger::endl;
     
 #ifdef DEBUG_ADMINQ
-    cout <<  "  uknown opcode=" << d->opcode << logger::endl;
+    cout <<  "  unknown opcode=" << d->opcode << logger::endl;
 #endif
     // desc_complete(I40E_AQ_RC_ESRCH);
     desc_complete(0);
