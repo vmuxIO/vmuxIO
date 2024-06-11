78e62a91ec2db9d0a2b5fa9a36b62abc1f7251bb
Pogobanane <aenderboy@gmx.de>
2024-06-11 16:47:43 +0000

    Revert "guest-extkern-config: attempt to fix docker kernel modules"
    This reverts commit c09d896247fcb97e765250e6a25ffd16e9a7408f.

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,6 +1,5 @@
 { pkgs, lib, ... }: {
   virtualisation.docker.enable = lib.mkDefault true; # socket activated only
-  networking.nftables.enable = true;
   environment.systemPackages = with pkgs; [
     docker-compose
   ];
