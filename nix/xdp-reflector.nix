cfff6f01fad85a19c9300bb2a9957c098ebd70c2
Pogobanane <aenderboy@gmx.de>
2024-07-03 17:10:36 +0000

    xdp-reflector: make pure_reflectors own MAC overridable

--- nix/xdp-reflector.nix
+++ nix/xdp-reflector.nix
@@ -1,6 +1,7 @@
 {xdp-reflector-src, pkgs, ...}: pkgs.clangStdenv.mkDerivation {
   pname = "xdp-reflector";
   version = "2022.08.03";
+  LOCAL_MAC = "{ 0x55, 0x55, 0x55, 0x55, 0x55, 0x55 }";
 
   src = xdp-reflector-src;
 
@@ -12,11 +13,17 @@
   ];
 
   hardeningDisable = [ "all" ];
+
+  buildPhase = ''
+    make OUR_MAC="$LOCAL_MAC"
+  '';
+
   #dontInstall = true;
   installPhase = ''
     mkdir -p $out/lib
     ls -lah
     cp reflector.o $out/lib
+    cp pure_reflector.o $out/lib
   '';
 
   # someone added some --gcc-toolchain arg which is of course not consumed by
