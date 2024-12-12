8160763ae3115ae1226cbf95294527c79119c766
Pogobanane <aenderboy@gmx.de>
2024-12-12 14:53:31 +0000

    measure_mediation: use 1 VM for brief (-b)

--- test/src/root.py
+++ test/src/root.py
@@ -4,4 +4,4 @@ from os import path
 TEST_ROOT = Path(__file__).parent.resolve().parent.resolve()
 PROJECT_ROOT = TEST_ROOT.parent
 
-QEMU_BUILD_DIR = "/scratch/okelmann/vmuxIO/qemu/bin" # TODO
+QEMU_BUILD_DIR = PROJECT_ROOT / "qemu" / "bin"
