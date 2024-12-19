f83c843cc0d291837c79a6f077633e9abfa01566
Pogobanane <aenderboy@gmx.de>
2024-12-19 10:11:09 +0000

    moongen-lachnit cleanup

--- nix/moongen-lachnit.nix
+++ nix/moongen-lachnit.nix
@@ -52,6 +52,8 @@ pkgs.stdenv.mkDerivation {
     ls -la ./libmoon
     patchShebangs ./libmoon/build.sh ./build.sh
 
+
+    # patch libmoon's include dirs
     substituteInPlace ./libmoon/build.sh \
       --replace "./bind-interfaces.sh \''${FLAGS}" "echo skipping bind-interfaces.sh"
     substituteInPlace ./libmoon/CMakeLists.txt \
@@ -72,6 +74,8 @@ pkgs.stdenv.mkDerivation {
       ''${CMAKE_CURRENT_SOURCE_DIR}/deps/dpdk/lib/rcu
       ''${CMAKE_CURRENT_SOURCE_DIR}/deps/dpdk/lib/hash
       ''${CMAKE_CURRENT_SOURCE_DIR}/deps/dpdk/lib/pci'
+
+    # patch moongen's include dirs
     substituteInPlace ./CMakeLists.txt \
       --replace '{CMAKE_CURRENT_SOURCE_DIR}/libmoon/deps/dpdk/lib/librte_ethdev' \
       '{CMAKE_CURRENT_SOURCE_DIR}/libmoon/deps/dpdk/lib/librte_ethdev
@@ -88,56 +92,23 @@ pkgs.stdenv.mkDerivation {
       ''${CMAKE_CURRENT_SOURCE_DIR}/libmoon/deps/dpdk/lib/eal/linux/include
       ''${CMAKE_CURRENT_SOURCE_DIR}/libmoon/deps/dpdk/lib/eal/x86/include'
 
+    # add libraries to link with moongen
     substituteInPlace ./CMakeLists.txt \
       --replace ' ''${dpdk_STATIC_LDFLAGS}' \
       ' rte_net      rte_ethdev       rte_distributor    rte_mbuf      rte_net_bond       rte_net_ring     rte_power
-  rte_acl      rte_eal          rte_kvargs     rte_mempool   rte_mempool_ring   rte_net_e1000        rte_net_virtio   rte_ring
-  rte_cfgfile  rte_hash         rte_lpm        rte_meter     rte_net_i40e   rte_net_ice    rte_net_iavf    rte_sched        rte_timer
-  rte_cmdline  rte_ip_frag      rte_pipeline   rte_net_ixgbe rte_mempool_stack  rte_port             rte_table
+        rte_acl      rte_eal          rte_kvargs     rte_mempool   rte_mempool_ring   rte_net_e1000        rte_net_virtio   rte_ring
+        rte_cfgfile  rte_hash         rte_lpm        rte_meter     rte_net_i40e   rte_net_ice    rte_net_iavf    rte_sched        rte_timer
+        rte_cmdline  rte_ip_frag      rte_pipeline   rte_net_ixgbe rte_mempool_stack  rte_port             rte_table
         rte_stack          rte_bus_vdev  rte_bus_pci        rte_pci              rte_cryptodev '
-      # ' -lrte_net -lrte_ethdev -lrte_eal -lrte_ring -lrte_mempool -lrte_mbuf -lrte_latencystats -lrte_cmdline -lrte_metrics -lrte_gso -lrte_gro -lrte_bpf -lrte_bitratestats -lbsd -lrte_net_ice -lrte_net_iavf'
 
+    # patch moongen's library search path
     substituteInPlace ./CMakeLists.txt \
       --replace '{CMAKE_CURRENT_SOURCE_DIR}/libmoon/deps/dpdk/x86_64-native-linux-gcc/lib' \
       '{CMAKE_CURRENT_SOURCE_DIR}/libmoon/deps/dpdk/x86_64-native-linux-gcc/lib
       ''${CMAKE_CURRENT_SOURCE_DIR}/libmoon/deps/dpdk/x86_64-native-linux-gcc/drivers'
 
-    # substituteInPlace ./libmoon/CMakeLists.txt \
-    #   --replace ' ''${dpdk_STATIC_LDFLAGS}' \
-    #   ' -lrte_net_ice -lrte_net '
-    substituteInPlace ./libmoon/CMakeLists.txt \
-      --replace ' ''${dpdk_STATIC_LDFLAGS}' \
-      ' rte_net      rte_ethdev       rte_distributor    rte_mbuf      rte_net_bond       rte_net_ring     rte_power
-  rte_acl      rte_eal          rte_kvargs     rte_mempool   rte_mempool_ring   rte_net_e1000        rte_net_virtio   rte_ring
-  rte_cfgfile  rte_hash         rte_lpm        rte_meter     rte_net_i40e   rte_net_ice    rte_net_iavf    rte_sched        rte_timer
-  rte_cmdline  rte_ip_frag      rte_pipeline   rte_net_ixgbe rte_mempool_stack  rte_port             rte_table
-        rte_stack          rte_bus_vdev  rte_bus_pci        rte_pci              rte_cryptodev '
-    # substituteInPlace ./libmoon/CMakeLists.txt \
-    #   --replace 'TARGET_LINK_LIBRARIES(libmoon' \
-    #   'TARGET_LINK_LIBRARIES('
-    # substituteInPlace ./libmoon/CMakeLists.txt \
-    #   --replace 'rt)' \
-    #   'libmoon rt)'
-    # substituteInPlace ./libmoon/CMakeLists.txt \
-    #   --replace 'ADD_EXECUTABLE(libmoon ''${FILES})' \
-    #   ' '
-    # substituteInPlace ./libmoon/CMakeLists.txt \
-    #   --replace 'rt)' \
-    #   'rt)
-    #   ADD_EXECUTABLE(libmoon ''${FILES})'
 
-    # substituteInPlace ./libmoon/CMakeLists.txt \
-    #   --replace '{dpdk_STATIC_LINK_DIRS}' \
-    #   '{dpdk_STATIC_LINK_DIRS}
-    #   ''${CMAKE_CURRENT_SOURCE_DIR}/deps/dpdk/x86_64-native-linux-gcc/drivers'
-
-    # substituteInPlace ./libmoon/build.sh \
-    #   --replace 'NUM_CPUS=$(cat /proc/cpuinfo  | grep "processor\\s: " | wc -l)' \
-    #   'NUM_CPUS=1'
-
-    # substituteInPlace ./libmoon/deps/dpdk/drivers/net/ice/base/ice_switch.h \
-    #   --replace 'bool ice_is_vsi_valid(struct' \
-    #     'extern bool ice_is_vsi_valid(struct'
+    # patch dpdk to export symbols used by libmoon
 
     substituteInPlace ./libmoon/deps/dpdk/drivers/net/ice/version.map \
       --replace 'local: *;' \
@@ -168,6 +139,8 @@ pkgs.stdenv.mkDerivation {
     i40e_aq_get_link_info;
          '
 
+    # patch dpdk to use our pinned e810 firmware
+
     substituteInPlace ./libmoon/deps/dpdk/drivers/net/ice/ice_ethdev.h \
       --replace '#define ICE_PKG_FILE_DEFAULT "/lib/firmware/intel/ice/ddp/ice.pkg"' \
       '#define ICE_PKG_FILE_DEFAULT "${linux-firmware-pinned}/lib/firmware/intel/ice/ddp/ice-1.3.26.0.pkg"'
@@ -176,11 +149,8 @@ pkgs.stdenv.mkDerivation {
       '#define ICE_PKG_FILE_SEARCH_PATH_DEFAULT "${linux-firmware-pinned}/lib/firmware/intel/ice/ddp/"'
   '';
 
-  NIX_LDFLAGS = " -yice_is_vsi_valid --verbose ";
-
   buildPhase = ''
-    # export dpdk_STATIC_LDFLAGS=" -lrte_eal -lrte_ring -lrte_mempool -lrte_ethdev -lrte_mbuf -lrte_net -lrte_latencystats -lrte_cmdline -lrte_net_bond -lrte_metrics -lrte_gso -lrte_gro -lrte_net_ixgbe -lrte_net_i40e -lrte_net_bnxt -lrte_net_dpaa -lrte_bpf -lrte_bitratestats -ljansson -lbsd"
-    NIX_DEBUG=1 ./build.sh
+    ./build.sh
   '';
 
   hardeningDisable = [ "all" ];
@@ -204,8 +174,6 @@ pkgs.stdenv.mkDerivation {
     cp -r libmoon/deps/dpdk/x86_64-native-linux-gcc/drivers $out/lib/dpdk
     mkdir -p $out/lib/luajit
     cp -r libmoon/deps/luajit/usr/local/lib $out/lib/luajit
-    # mkdir -p $out/lib/highwayhash
-    # cp -r libmoon/deps/highwayhash/lib $out/lib/highwayhash
 
     # autopatchelfHook?
     patchelf --shrink-rpath --allowed-rpath-prefixes /nix/store $out/bin/MoonGen
@@ -214,7 +182,6 @@ pkgs.stdenv.mkDerivation {
     patchelf --add-rpath $out/lib/dpdk/lib $out/bin/MoonGen
     patchelf --add-rpath $out/lib/dpdk/drivers $out/bin/MoonGen
     patchelf --add-rpath $out/lib/luajit/usr/local/lib $out/bin/MoonGen
-    # patchelf --add-rpath $out/lib/highwayhash/lib $out/bin/MoonGen
   '';
 
   dontFixup = true;
